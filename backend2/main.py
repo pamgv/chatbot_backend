3dfe54ce2afe58dd210fcaf5e7cd1347fd6e8761
pamgv <112966514+pamgv@users.noreply.github.com>
2025-11-17 12:07:43 -0700

    Refactor main.py for improved readability

--- backend2/main.py
+++ backend2/main.py
@@ -1,6 +1,6 @@
 #------- Nuevo código ----------#
 from fastapi import FastAPI
-from middlewares.rate_limit import rate_limi
+from middlewares.rate_limit import rate_limit
 from fastapi.middleware.cors import CORSMiddleware
 from routes.chatbot_routes import router as chatbot_router
 from routes.user_routes import router as user_router
@@ -45,6 +45,7 @@ async def apply_rate_limit(request, call_next):
 @app.get("/")
 def root():
     return {"message": "🚀 Chatbot backend modular running and connected to MongoDB!"}
+
 
 
 
